721c7aed3699384a67606dbcad1ed7894855eee7
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2020-11-09 17:59:13 +0530

    Images_fix

--- Hack/Personaldetails.xaml.cs
+++ Hack/Personaldetails.xaml.cs
@@ -77,6 +77,7 @@ namespace Hack
                 catch(Exception ex)
                 {
                     MessageBox.Show(ex.Message);
+                    
                 }
             }
             
